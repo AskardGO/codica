cd5e5ff086608af0f30bd7dbce1e4e84986802b0
AskardGO <66526283+AskardGO@users.noreply.github.com>
2021-12-02 18:03:42 +0300

    update weather card

--- src/Pages/Weather/Card/index.tsx
+++ src/Pages/Weather/Card/index.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import {Card, CardContent, CardActions, Typography, Button} from "@mui/material";
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 import styles from './Card.module.sass'
 
-type Props = {
-
-};
+type Props = {};
 export const WeatherCard = () => {
     return (
-        <Card className={styles.container} sx={{ minWidth: 200, minHeight: 250 }}>
-            <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    Word of the Day
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
+        <div className={styles.card}>
+            <div className={styles.cardTools}>
+                <RefreshIcon/>
+            </div>
+            <div className={styles.cardCity}>Eindhoven</div>
+            <div className={styles.cardDate}>29 september 2015</div>
+            <div className="weather">
+                <div className={styles.cardSun}> </div>
+                <div className={styles.cardTemp}>12°C</div>
+            </div>
+        </div>
+
     );
 };
